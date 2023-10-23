3380cbf1cccf1763f6d6d4b09da49e1fe633be69
Catharina Oliveira <catharinaoliveira02@gmail.com>
2023-10-23 18:04:24 -0300

    Update cadastroPaciente.js

--- front-end/js/cadastroPaciente.js
+++ front-end/js/cadastroPaciente.js
@@ -1,15 +1,32 @@
-const cadastrarPaciente = document.getElementById('cadastroPaciente')
-
-
-
+const cadastrarPaciente = document.getElementById('cadastro_pac')
 
 let formData = new FormData();
 
 cadastrarPaciente.addEventListener('submit', event => {
         event.preventDefault()
-
         let formData = new FormData(cadastrarPaciente)
         const data = Object.fromEntries(formData)
 
-        console.log(data)
+  
+
+        var id = "id" + Math.random().toString(16).slice(2)
+        var mysql = require('mysql');
+
+        var con = mysql.createConnection({
+          host: "localhost",
+          user: "root",
+          password: "1997",
+          database: "todolist"
+        });
+        
+        con.connect(function(err) {
+          if (err) throw err;
+          console.log("Connected!");
+          var sql = "INSERT INTO paciente (codigo_pac, nome_pac, data_nasc_pac, cpf_pac, email_pac, celular_pac, endereco_pac, sexo_pac) VALUES ('"+ id +"','"+data.nome_pac+"','"+data_nasc_pac+"','"+data.cpf_pac +"','"+data.email_pac+"','"+data.celular_pac+"','"+data.endereco+"','"+data.sexo_pac +"')";
+          con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+          });
+        });
+
     })
